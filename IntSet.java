f5441768ef67e3f68ee23c19bb8b44b6c2546754
Austin Schwartz <schwar12@purdue.edu>
2016-02-09 22:11:08 -0500

    Doneee

--- IntSet.java
+++ IntSet.java
@@ -1,5 +1,4 @@
 public interface IntSet {
-  // These methods need to be threadsafe (i.e.well-synchronized).
   public boolean insert(int x);
   public boolean remove(int x);
   public boolean contain(int x);
--- Main.java
+++ Main.java
@@ -3,44 +3,34 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     int t = 4, u = 50, i = 10000, d = 3000;
-    String s = "coarse";
-    for (int k = 0; k < args.length - 1; k++) {
-      if (args[k].charAt(0) == '-') {
-        switch (args[k].charAt(1)) {
-          case 't':
-            t = Integer.parseInt(args[k + 1]);
-            break;
-          case 'u':
-            u = Integer.parseInt(args[k + 1]);
-            break;
-          case 'i':
-            i = Integer.parseInt(args[k + 1]);
-            break;
-          case 'd':
-            d = Integer.parseInt(args[k + 1]);
-            break;
-          case 'b':
-            s = args[k + 1];
-      }
-     }
-    }
     try {
+      String s = "coarse";
+      for (int k = 0; k < args.length - 1; k++) {
+        if (args[k].charAt(0) == '-') {
+          switch (args[k].charAt(1)) {
+            case 't':
+              t = Integer.parseInt(args[k + 1]);
+              break;
+            case 'u':
+              u = Integer.parseInt(args[k + 1]);
+              break;
+            case 'i':
+              i = Integer.parseInt(args[k + 1]);
+              break;
+            case 'd':
+              d = Integer.parseInt(args[k + 1]);
+              break;
+            case 'b':
+              s = args[k + 1];
+        }
+       }
+      }
       (new Benchmark(t, u, i, d, s)).performBenchmark();
     } catch (Exception e) {
       System.out.println("Argument format: java Main -t 8 -d 3000 -u 100 -i 10000 -b coarse");
     }
-     
-    //Benchmark(8, 1, 10000, 3000);
   }
-  // Benchmark(threads, updateratio, initial size, duration in ms, test)
-  public static void Benchmark(int t, int u, int l, int d) {
-    (new Benchmark(t, u, l, d, "listset")).performBenchmark();
-    (new Benchmark(t, u, l, d, "coarse")).performBenchmark();
-    (new Benchmark(t, u, l, d, "hoh")).performBenchmark();
-    (new Benchmark(t, u, l, d, "optimistic")).performBenchmark();
-  }
-
-
+  /*
   public static boolean Test(IntSet set) {
     boolean good = true;
     set.insert(50);
@@ -54,4 +44,5 @@ public class Main {
     good |= set.contain(238);
     return good;
   }
+  */
 }
